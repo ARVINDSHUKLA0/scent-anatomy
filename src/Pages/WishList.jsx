507a125b001928b072aed680d912dcc9fb8abe30
ARVINDSHUKLA0 <shuklaarvind2521@gmail.com>
2025-04-22 16:58:16 +0530

    updated project files

--- src/Pages/WishList.jsx
+++ src/Pages/WishList.jsx
@@ -11,14 +11,14 @@ const WishList = () => {
   return (
     <>
       <Navbar />
-      <section className='my-4'>
-        <div className="row m-0">
+      <section className='my-4 wishlist-conatiner'>
+        <div className="row align-items-center m-0">
           {
             dataWislist.length === 0 ? (
-              <div className='my-100 text-center'>
+              <div className='my-100 text-center wishlist-not-data-showing'>
                 <h3 className='text-danger custom-font-degin'>Noting found in wishlist</h3>
                 <Link to="/shop" className='text-decoration-none text-white'>
-                  <button className='btn-normal custom-width-btn my-3 text-capitalize '>
+                  <button className='btn-normal custom-width-btn w-100 my-3 text-capitalize '>
                     continue shopping
                   </button>
                 </Link>
@@ -29,7 +29,7 @@ const WishList = () => {
                 <div key={index} className='col-lg-3 col-md-4 col-sm-6 col-12'>
                   <div className='wishlist-min-height'>
                     <div className='position-relative px-2 py-2'>
-                      <img src={wishListdata.thumbnailImage} width={300} alt="" />
+                      <img className='img-fluid' src={wishListdata.thumbnailImage} alt="" />
                       <h6 className='m-0 text-wrap py-2'>{wishListdata.title}</h6>
                       <p className='fs-small'> &#x20B9; {wishListdata.price}</p>
                       <div className='wishlist-remove-postion'>
