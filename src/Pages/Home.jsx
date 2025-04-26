81df198179d11aa48bf857e5f6f977a89b7deaef
ARVINDSHUKLA0 <shuklaarvind2521@gmail.com>
2025-04-26 11:14:39 +0530

    update

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -124,8 +124,8 @@ const Home = () => {
                       </Link>
                       <div
                         className="position-absolute product-wrapper d-flex align-items-center justify-content-center ">
-                        <span className="d-flex align-items-center h-100 ">
-                          <p className="bg-dark text-white  mb-0  h-100 px-3 pt-1  text-uppercase fs-small">Quick Look</p>
+                        <span className="d-flex align-items-center h-100 pe-3 ">
+                          <p className="bg-dark text-white  mb-0  h-100 px-lg-3 px-2 pt-1  text-center text-uppercase fs-small">Quick Look</p>
                           <Link onClick={() => WislistFunc(itemValue.id)} >
                             <i className="fa-regular fa-heart h-100 custom-padding-icon-heart  bg-secondary text-white fs-mediam-font-size "></i>
                           </Link>
